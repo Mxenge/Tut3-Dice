d5f2b66ac037c2661d43df19c7d1d19ae395525f
MXENGE <213571329@stu.ukzn.ac.za>
2016-03-06 18:18:08 +0200

    Function declaration and implementation

--- class dice/Dice.h
+++ class dice/Dice.h
@@ -1,9 +1,18 @@
+#include <iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
 class Dice 
 {
 	private:
-			int dice;
+		int dice;
+		int numRolls;
 	public:
 		Dice(); // constructor
 	    ~Dice(); // deconstructor
 		int roll(); // memeber function
+		void display();
+		void reset();
 }; 
